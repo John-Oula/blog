e3fc44e18b8627d9203b643985457ee61899a7a8
John-Oula <johnoula@icloud.com>
2022-02-02 00:25:14 +0800

    Update Article.js

--- Components/Article.js
+++ Components/Article.js
@@ -11,17 +11,17 @@ function Article({data}) {
 
                 <Box border={`solid`} p={3} borderWidth={`1px`}
                      borderColor={` #d6d9dc`} flexDirection={`column`} >
-                    <Heading color={`#287b4f`} mb={3} size={`md`} as={`h5`}>{data.post?.title}</Heading>
+                    <Heading color={`#287b4f`} mb={3} size={`md`} as={`h5`}>{data?.post?.title}</Heading>
                     <Flex mt={`5pt`} mb={`5pt`} alignItems={`center`}>
                         <TimeIcon mr={3}/>
                         <Moment format="D MMM YYYY" >
 
-                            <Text fontSize={`sm`}> { data.post?._updatedAt}</Text>
+                            <Text fontSize={`sm`}> { data?.post?._updatedAt}</Text>
                         </Moment>
 
                     </Flex>
 
-                    <PortableText data={data.post?.body} />
+                    <PortableText data={data?.post?.body} />
                 </Box>
             </Flex>
         </Box>
